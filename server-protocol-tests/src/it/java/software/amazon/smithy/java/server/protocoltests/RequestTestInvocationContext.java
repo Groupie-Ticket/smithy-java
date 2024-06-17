88d4590c12f2beb407ea260760c3ba07199b3717
Adam Thomas <adamthom@amazon.com>
2024-06-17 11:49:15 -0700

    Deserialize protocol test expectations
    Instead of doing a weird structural comparison between an ObjectNode-as-
    document from the test case, build a Document implementation that we
    can use with a ShapeSerializer so that our tests can simply use
    assertEquals and failed test results look normal to humans.

--- server-protocol-tests/src/it/java/software/amazon/smithy/java/server/protocoltests/RequestTestInvocationContext.java
+++ server-protocol-tests/src/it/java/software/amazon/smithy/java/server/protocoltests/RequestTestInvocationContext.java
@@ -7,16 +7,25 @@ package software.amazon.smithy.java.server.protocoltests;
 
 import java.net.URI;
 import java.util.List;
+import java.util.function.Supplier;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
-import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.java.runtime.core.schema.SerializableShape;
+import software.amazon.smithy.java.runtime.core.schema.ShapeBuilder;
+import software.amazon.smithy.java.runtime.core.serde.document.ProtocolTestDocument;
 import software.amazon.smithy.protocoltests.traits.HttpRequestTestCase;
 
-record RequestTestInvocationContext(URI endpoint, HttpRequestTestCase testCase, MockOperation mockOperation)
+record RequestTestInvocationContext(
+    URI endpoint,
+    HttpRequestTestCase testCase,
+    MockOperation mockOperation,
+    Supplier<ShapeBuilder<?>> inputBuilder,
+    SerializableShape manualExpectation
+)
     implements TestTemplateInvocationContext {
 
     @Override
@@ -91,7 +100,7 @@ record RequestTestInvocationContext(URI endpoint, HttpRequestTestCase testCase, 
                     ParameterContext paramCtx,
                     ExtensionContext extensionCtx
                 ) throws ParameterResolutionException {
-                    return paramCtx.getParameter().getType().equals(ObjectNode.class);
+                    return paramCtx.getParameter().getType().equals(SerializableShape.class);
                 }
 
                 @Override
@@ -99,7 +108,12 @@ record RequestTestInvocationContext(URI endpoint, HttpRequestTestCase testCase, 
                     ParameterContext paramCtx,
                     ExtensionContext extensionCtx
                 ) throws ParameterResolutionException {
-                    return testCase.getParams();
+                    if (manualExpectation() != null) {
+                        return manualExpectation();
+                    }
+                    var builder = inputBuilder.get();
+                    new ProtocolTestDocument(testCase.getParams()).deserializeInto(builder);
+                    return builder.build();
                 }
             }
         );
