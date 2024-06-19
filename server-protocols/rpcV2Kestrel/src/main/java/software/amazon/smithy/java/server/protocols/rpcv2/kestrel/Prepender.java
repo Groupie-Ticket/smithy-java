0fd1e2cf572866508598bf7733440f1bd03aa344
Adwait Kumar Singh <adwsingh@amazon.com>
2024-06-19 01:17:25 -0700

    Apply spotless

--- server-protocols/rpcV2Kestrel/src/main/java/software/amazon/smithy/java/server/protocols/rpcv2/kestrel/Prepender.java
+++ server-protocols/rpcV2Kestrel/src/main/java/software/amazon/smithy/java/server/protocols/rpcv2/kestrel/Prepender.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package software.amazon.smithy.java.server.protocols.rpcv2.kestrel;
 
 import java.util.concurrent.Flow;
