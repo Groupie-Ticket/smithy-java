71319daff78274ba0d477631d37a92e1c74b3364
Adwait Kumar Singh <adwsingh@amazon.com>
2024-06-19 01:17:25 -0700

    Apply spotless

--- core/src/main/java/software/amazon/smithy/java/runtime/core/serde/InitialMessageDecoder.java
+++ core/src/main/java/software/amazon/smithy/java/runtime/core/serde/InitialMessageDecoder.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package software.amazon.smithy.java.runtime.core.serde;
 
 import java.nio.ByteBuffer;
